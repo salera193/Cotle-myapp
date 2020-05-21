5400842de555e594410c99f8ae9ca92555c850b6
salera193 <ryokke1419@gmail.com>
2020-05-21 16:44:37 +0900

    now making main fanction

--- Cotle/urls.py
+++ Cotle/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.rec_list, name='rec_list'),
+    path('rec/<int:pk>/', views.rec_detail, name='rec_detail'),
     ]
 
--- Cotle/views.py
+++ Cotle/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Recruit
 from django.utils import timezone 
+from django.shortcuts import get_object_or_404
 
 def rec_list(request):
     
@@ -8,3 +9,10 @@ def rec_list(request):
     
     params ={'Recs': Recs}
     return render(request, 'Cotle/rec_list.html', params)
+
+def rec_detail(request, pk):
+    rec = get_object_or_404(Recruit, pk=pk)
+    params={
+        'rec':rec
+        }
+    return render(request, 'Cotle/rec_detail.html', params)    
